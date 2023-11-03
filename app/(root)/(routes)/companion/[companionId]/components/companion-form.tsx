675266caa60d3224fc62b3d305c0c4c9dc408df5
Tianxiang Ren <ren.tianx@northeastern.edu>
2023-11-02 22:58:09 -0700

    updated apis

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
@@ -20,6 +21,8 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -75,11 +78,34 @@ const CompanionForm = ({ categories, initialData }: CompanionFormProps) => {
       categoryId: undefined,
     },
   });
+  const router = useRouter();
+  const {toast} = useToast();
 
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+      try{
+        // update companion func
+        if(initialData){
+          await axios.patch(`/api/companion/${initialData.id}`, values);
+        } else {
+          // create companion func
+          await axios.post('/api/companion', values);
+        }
+
+        toast({
+          description: "Success!"
+        });
+
+        router.refresh();
+        router.push("/");
+
+      } catch (error) {
+        toast({
+          variant: "destructive",
+          description: "Something went wrong!"
+        });
+      }
   };
 
   return (
