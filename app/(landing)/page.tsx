d068d1813462dc5f45fa89b19b0446495d3b146f
Tianxiang Ren <ren.tianx@northeastern.edu>
2023-12-21 14:31:09 -0800

    updated landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,17 @@
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
+import Footer from "@/components/footer";
+
+const LandingPage = () => {
+  return ( 
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+      <Footer/>
+    </div>
+   );
+}
+ 
+export default LandingPage;
